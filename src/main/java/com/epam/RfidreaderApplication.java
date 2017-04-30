9e102e48bebd6caba2497bf3073fd29e89aa0f35
Boros Tamás <boros.tamas456@gmail.com>
2017-04-30 20:38:41 +0200

    Spring Boot base

---
+++ src/main/java/com/epam/RfidreaderApplication.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidreaderApplication.class, args);
+	}
+}
